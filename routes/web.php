e9d3618daede6b8f7e45e2ad3fe0d6af93084855
Dinan <116496595+DinanII@users.noreply.github.com>
2023-12-15 13:14:16 +0100

    index

--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', [BlogController::class, 'index'])->middleware(['auth', 'verified']);
 // Dashboard
-Route::get('/dashboard', function () {
+Route::get('/', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
